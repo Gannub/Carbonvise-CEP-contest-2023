f7345ea38df6063126fb13ef0bc52381eeb5acfa
PuffinPole <134520798+PuffinPole@users.noreply.github.com>
2023-07-26 00:17:30 +0700

    Delete profiles directory

--- profiles/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from profiles.models import Profile, CreditSession, CreditHistory
-
-
-admin.site.register(Profile)
-admin.site.register(CreditSession)  
-admin.site.register(CreditHistory)  
