438a5453c909878ba9f6b1125837be845e4d32f2
MooYongArin <yongariyata@gmail.com>
2023-07-27 13:08:32 +0700

    add responsive scroll in profilePage

--- src/profile.js
+++ src/profile.js
@@ -64,7 +64,69 @@ const Profile = () => {
                     <div className="project-container">
                         <div className="project-title">Projects</div>
                         <div className="project-content">
-                        <div className="wrapper-project">
+                        <div className="wrapper-project snaps-inline">
+                    <Link to="/market-inside">
+                    <div className="product1-profile">
+                        <div className="related-pic-1-profile">
+                            <div className="subcategory-each">
+                                <Link to="/market/renewable energy">Renewable energy</Link>
+                            </div>
+                        </div>
+                        <div className="about-pic-profile">
+                            <div className="about-pic-left">
+                                <div className="type-profile">EcoPower</div>
+                                <div className="rating">
+                                    <img src={leaf} alt="" />
+                                    5.0(52 Reviews)
+                                    </div>
+                            </div>
+                            <div className="about-pic-right">
+                                <div className="num-profile">324</div>
+                                <div className="subunit">Chestnuts</div>
+                            </div>
+                        </div>
+                    </div>
+                    </Link>
+                    <div className="product2-profile">
+                        <div className="related-pic-2-profile">
+                            <div className="subcategory-each">
+                                <Link to="/market/renewable energy">Solar energy</Link>
+                            </div>
+                        </div>
+                        <div className="about-pic-profile">
+                            <div className="about-pic-left">
+                                <div className="type-profile">Embrace Solar Power</div>
+                                <div className="rating">
+                                    <img src={leaf} alt="" />
+                                    4.9(39 Reviews)
+                                    </div>
+                            </div>
+                            <div className="about-pic-right">
+                                <div className="num-profile">500</div>
+                                <div className="subunit">Chestnuts</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="product3-profile">
+                        <div className="related-pic-3-profile">
+                            <div className="subcategory-each">
+                                <Link to="/market/renewable energy">Waste Heat Recovery</Link>
+                            </div>
+                        </div>
+                        <div className="about-pic-profile">
+                            <div className="about-pic-left">
+                                <div className="type-profile">Embrace Solar Power</div>
+                                <div className="rating">
+                                    <img src={leaf} alt="" />
+                                    1.9(70 Reviews)
+                                    </div>
+                            </div>
+                            <div className="about-pic-right">
+                                <div className="num-profile">2903</div>
+                                <div className="subunit">Chestnuts</div>
+                            </div>
+                        </div>
+                    </div>
                     <Link to="/market-inside">
                     <div className="product1-profile">
                         <div className="related-pic-1-profile">
@@ -134,8 +196,96 @@ const Profile = () => {
                 <div className="blogs-profile">
                     <div className="blogs-profile-container">
                         <div className="blogs-text">Blogs</div>
-                        <div className="blogs-content">
+                        <div className="blogs-content snap-inline">
                         <div className="secondblog-profile">
+                        <div className="container-eachblog-profile">
+                        <div className="nav-eachblog-profile">
+                            <div className="time-profile">16 hour ago</div>
+                            <div className="three-dot"><img src={threeDot} alt="" /></div>
+                        </div>
+                        <div className="underline-eachblog"></div>
+                        <div className="below-eachblog">
+                            <div className="title-blog">Revitalize Your Morning Routine...</div>
+                            <div className="text-blog">Start your day off right with these refreshing and nutritious vegan 
+                            smoothie recipes. Packed with vitamins, minerals, and plant-based protein</div>
+                            <div className="tips-readmore">
+                                <div className="tips"> 
+                                    <Link to="/blogs/events">Events</Link>
+                                </div>
+                                <div className="read-more">
+                                    <Link to="/blogs/read-more">Read more</Link>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="thirdblog-profile">
+                        <div className="container-eachblog-profile">
+                        <div className="nav-eachblog-profile">
+                            <div className="time-profile">18 hour ago</div>
+                            <div className="three-dot"><img src={threeDot} alt="" /></div>
+                        </div>
+                        <div className="underline-eachblog"></div>
+                        <div className="below-eachblog">
+                            <div className="title-blog">Mastering the Art of Communication</div>
+                            <div className="text-blog">Enhance your communication skills by understanding different 
+                            communication styles, active listening techniques, and fostering effective dialogue</div>
+                            <div className="tips-readmore">
+                                <div className="tips"> 
+                                    <Link to="/blogs/events">Events</Link>
+                                </div>
+                                <div className="read-more">
+                                    <Link to="/blogs/read-more">Read more</Link>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="secondblog-profile">
+                        <div className="container-eachblog-profile">
+                        <div className="nav-eachblog-profile">
+                            <div className="time-profile">16 hour ago</div>
+                            <div className="three-dot"><img src={threeDot} alt="" /></div>
+                        </div>
+                        <div className="underline-eachblog"></div>
+                        <div className="below-eachblog">
+                            <div className="title-blog">Revitalize Your Morning Routine...</div>
+                            <div className="text-blog">Start your day off right with these refreshing and nutritious vegan 
+                            smoothie recipes. Packed with vitamins, minerals, and plant-based protein</div>
+                            <div className="tips-readmore">
+                                <div className="tips"> 
+                                    <Link to="/blogs/events">Events</Link>
+                                </div>
+                                <div className="read-more">
+                                    <Link to="/blogs/read-more">Read more</Link>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="thirdblog-profile">
+                        <div className="container-eachblog-profile">
+                        <div className="nav-eachblog-profile">
+                            <div className="time-profile">18 hour ago</div>
+                            <div className="three-dot"><img src={threeDot} alt="" /></div>
+                        </div>
+                        <div className="underline-eachblog"></div>
+                        <div className="below-eachblog">
+                            <div className="title-blog">Mastering the Art of Communication</div>
+                            <div className="text-blog">Enhance your communication skills by understanding different 
+                            communication styles, active listening techniques, and fostering effective dialogue</div>
+                            <div className="tips-readmore">
+                                <div className="tips"> 
+                                    <Link to="/blogs/events">Events</Link>
+                                </div>
+                                <div className="read-more">
+                                    <Link to="/blogs/read-more">Read more</Link>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="secondblog-profile">
                         <div className="container-eachblog-profile">
                         <div className="nav-eachblog-profile">
                             <div className="time-profile">16 hour ago</div>
